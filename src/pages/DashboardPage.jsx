0f85777c50de7b4574d18103f0ab92130bb802e1
Lavya <lavyajain.dev@gmail.com>
2025-09-19 02:12:51 +0530

    Setup complete

---
+++ src/pages/DashboardPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function DashboardPage () {
+    return (
+        <div>
+            <h2>Merchant Dashboard</h2>
+            <p>Welcome! Here you will see your transactions and API keys.</p>
+        </div>
+
+    );
+}
+export default DashboardPage;
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function LoginPage () {
+    return(
+        <div>
+            <h2>Login to Your Merchant Account</h2>
+        </div>
+    );
+}
+export default LoginPage;
